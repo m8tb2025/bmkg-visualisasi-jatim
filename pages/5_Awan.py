d4db8b3d30c8834ab28763e4d8e46daa6959ab65
m8tb2025 <tbmeteorologi@gmail.com>
2025-08-28 00:30:05 +0700

    Update 5_Awan.py

--- pages/5_Awan.py
+++ pages/5_Awan.py
@@ -1,28 +1,30 @@
-from streamlit_folium import st_folium
-import folium
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import pandas as pd
 
-# Buat peta
-m = folium.Map(location=[-7.5, 112.5], zoom_start=7)
+# === 1. Buka data (misal GRIB/NetCDF) ===
+ds = xr.open_dataset("cloud_data.nc")   # ganti sesuai file data
+low_cloud = ds["cld_low"]               # variabel awan rendah (0–3 km)
 
-# Tambahkan legend custom (CSS biar lebih kecil)
-legend_html = """
-<div style="
-position: fixed; 
-bottom: 50px; left: 50px; width: 150px; 
-background-color: white; 
-border:2px solid grey; 
-z-index:9999; 
-font-size:12px;
-padding: 5px;
-">
-<b>Legenda Awan Rendah</b><br>
-<i style="background:red;width:10px;height:10px;float:left;margin-right:5px;"></i> Cb (Cumulonimbus)<br>
-<i style="background:blue;width:10px;height:10px;float:left;margin-right:5px;"></i> Cu (Cumulus)<br>
-<i style="background:gray;width:10px;height:10px;float:left;margin-right:5px;"></i> St (Stratus)<br>
-</div>
-"""
+# === 2. Daftar waktu per 3 jam ===
+times = pd.date_range(start=str(low_cloud.time.values[0]), 
+                      end=str(low_cloud.time.values[-1]), freq="3H")
 
-m.get_root().html.add_child(folium.Element(legend_html))
+# === 3. Looping tiap jam ===
+for t in times:
+    data_t = low_cloud.sel(time=t)
 
-# Tampilkan di Streamlit
-st_folium(m, width=700, height=500)
+    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()}, figsize=(8,6))
+    ax.coastlines()
+    
+    # === 4. Plot data awan rendah ===
+    im = data_t.plot(ax=ax, cmap="gray_r", transform=ccrs.PlateCarree(),
+                     add_colorbar=False)
+    
+    # === 5. Tambahkan judul waktu ===
+    plt.title(f"Low Clouds (Cu, Cb, St, Sc)\nValid: {t:%Y-%m-%d %H UTC}", fontsize=12)
+    
+    # === 6. Simpan hasil ===
+    plt.savefig(f"lowcloud_{t:%Y%m%d%H}.png", dpi=150, bbox_inches="tight")
+    plt.clo
